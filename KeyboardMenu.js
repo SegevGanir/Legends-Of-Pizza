333038d7f8dc5aae8108fa4e7047dd19945c6c33
SegevGanir <Segev@ganir.net>
2023-05-26 04:00:16 +0000

    modified:   KeyboardMenu.js

--- KeyboardMenu.js
+++ KeyboardMenu.js
@@ -8,6 +8,16 @@ class KeyboardMenu {
 
   setOptions(options) {
     this.options = options;
+    this.element.innerHTML = this.options.map((option, index) => {
+      return (`
+        <div class="option">
+          <button data-button = "${index}" data-description = "${option.description}">
+           ${option.label}
+          </button>
+          <span class="right">${o.right ? o.right() : ""}</span>
+        </div>
+      `)
+    }).join("")
 
   }
 
